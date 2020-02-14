a06ad05d1b2bdc567825cc2d46e22a63ba43d7de
chipytux <chipytux@gmail.com>
2020-02-14 00:39:05 +0000

    inicio do uso do multer

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 import UserController from './app/controllers/userController';
 import SessionController from './app/controllers/sessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.show);
